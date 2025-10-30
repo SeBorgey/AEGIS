a2f6c76bec307b87f2b8b19e243a73bb85eeb9b2
Sergey Malyshev <malyshev.sa@phystech.edu>
2025-10-30 11:50:51 +0300

    easy dataset

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from agent import ProgrammingAgent
@@ -12,16 +13,29 @@ def main():
         print("Пожалуйста, запустите скрипт через './run.sh'")
         return
 
-    task = """
-Напиши мне калькулятор
-"""
+    #     task = """
+    # Напиши мне простой браузер
+    # """
 
-    print("Инициализация компонентов...")
-    llm_client = LLMClient(api_key=api_key)
-    code_executor = CodeExecutor(workspace_path="project_files")
-    agent = ProgrammingAgent(llm_client, code_executor)
+    #     print("Инициализация компонентов...")
+    #     llm_client = LLMClient(api_key=api_key)
+    #     code_executor = CodeExecutor(workspace_path="project_files")
+    #     agent = ProgrammingAgent(llm_client, code_executor)
 
-    agent.create_program(task)
+    #     agent.create_program(task)
+
+    with open("datasets/easy.json", "r", encoding="utf-8") as f:
+        tasks = json.load(f)
+
+    for task_item in tasks:
+        task = f"Напиши мне {task_item}"
+
+        print("Инициализация компонентов...")
+        llm_client = LLMClient(api_key=api_key)
+        code_executor = CodeExecutor(workspace_path=task_item)
+        agent = ProgrammingAgent(llm_client, code_executor)
+
+        agent.create_program(task)
 
 
 if __name__ == "__main__":
